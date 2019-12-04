cc45018c8324ab50d4a313aab722ae10e1f3b513
Elias Projahn <johrpan@gmail.com>
2019-12-04 20:52:52 +0100

    Add access to person editor to person selector
    The person editor can be reached via the FAB. If a new person is
    created, it will be selected.

--- lib/selectors/person.dart
+++ lib/selectors/person.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import '../backend.dart';
+import '../database.dart';
+import '../editors/person.dart';
 
 class PersonsSelector extends StatelessWidget {
   @override
@@ -12,24 +14,40 @@ class PersonsSelector extends StatelessWidget {
         title: Text('Select person'),
       ),
       body: StreamBuilder(
-          stream: backend.db.allPersons().watch(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return ListView.builder(
-                itemCount: snapshot.data.length,
-                itemBuilder: (context, index) {
-                  final person = snapshot.data[index];
+        stream: backend.db.allPersons().watch(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index) {
+                final person = snapshot.data[index];
 
-                  return ListTile(
-                    title: Text('${person.lastName}, ${person.firstName}'),
-                    onTap: () => Navigator.pop(context, person),
-                  );
-                },
-              );
-            } else {
-              return Container();
-            }
-          }),
+                return ListTile(
+                  title: Text('${person.lastName}, ${person.firstName}'),
+                  onTap: () => Navigator.pop(context, person),
+                );
+              },
+            );
+          } else {
+            return Container();
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () async {
+          final Person person = await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => PersonEditor(),
+                fullscreenDialog: true,
+              ));
+
+          if (person != null) {
+            Navigator.pop(context, person);
+          }
+        },
+      ),
     );
   }
 }
