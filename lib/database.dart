4b178752b9aeffa7c616b4946355994566f1c503
Elias Projahn <johrpan@gmail.com>
2020-03-21 14:34:45 +0100

    Redo how works are stored
    Before works where stored using a complicated tree structure where each
    part references its parent. Now it's a flat structure that keeps track
    of the relations using a new field named part_level. The corresponding
    query was simplified accordingly. Also, the updateWork function was
    rewritten.

--- lib/database.dart
+++ lib/database.dart
@@ -7,6 +7,16 @@ part 'database.g.dart';
 final _random = Random(DateTime.now().millisecondsSinceEpoch);
 int generateId() => _random.nextInt(0xFFFFFFFF);
 
+class WorkModel {
+  final Work work;
+  final List<int> instrumentIds;
+
+  WorkModel({
+    @required this.work,
+    @required this.instrumentIds,
+  });
+}
+
 @UseMoor(
   include: {
     'database.moor',
@@ -19,6 +29,14 @@ class Database extends _$Database {
   @override
   int get schemaVersion => 1;
 
+  // TODO: Remove this once https://github.com/simolus3/moor/issues/453 is fixed.
+  Selectable<Work> worksByComposer(int id) {
+    return customSelectQuery(
+        'SELECT DISTINCT A.* FROM works A, works B ON A.id = B.part_of WHERE A.composer = :id OR B.composer = :id',
+        variables: [Variable.withInt(id)],
+        readsFrom: {works}).map(_rowToWork);
+  }
+
   Future<void> updatePerson(Person person) async {
     await into(persons).insert(person);
   }
@@ -27,19 +45,26 @@ class Database extends _$Database {
     await into(instruments).insert(instrument);
   }
 
-  Future<void> updateWork(Work work, List<int> instrumentIds) async {
+  Future<void> updateWork(WorkModel model, List<WorkModel> parts) async {
     await transaction(() async {
-      await into(works).insert(work);
+      final workId = model.work.id;
+      await (delete(works)..where((w) => w.id.equals(workId))).go();
+      await (delete(works)..where((w) => w.partOf.equals(workId))).go();
 
-      await (delete(instrumentations)..where((i) => i.work.equals(work.id)))
-          .go();
+      Future<void> insertWork(WorkModel model) async {
+        await into(works).insert(model.work);
+        await batch((b) => b.insertAll(
+            instrumentations,
+            model.instrumentIds
+                .map((id) =>
+                    Instrumentation(work: model.work.id, instrument: id))
+                .toList()));
+      }
 
-      await into(instrumentations).insertAll(instrumentIds
-          .map((id) => Instrumentation(
-                work: work.id,
-                instrument: id,
-              ))
-          .toList());
+      await insertWork(model);
+      for (final part in parts) {
+        await insertWork(part);
+      }
     });
   }
 }
