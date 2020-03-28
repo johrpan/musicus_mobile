cbbc5bc7c6a853eb8020fa68b05a1959c6aa17cb
Elias Projahn <johrpan@gmail.com>
2020-03-28 10:24:32 +0100

    Settings: Add music library path setting

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../backend.dart';
+import '../selectors/files.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
@@ -13,6 +14,26 @@ class SettingsScreen extends StatelessWidget {
       ),
       body: ListView(
         children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.library_music),
+            title: Text('Music library path'),
+            subtitle: Text(backend.musicLibraryPath),
+            onTap: () async {
+              final path = await Navigator.push<String>(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => FilesSelector(
+                    mode: FilesSelectorMode.directory,
+                  ),
+                  fullscreenDialog: true,
+                ),
+              );
+
+              if (path != null) {
+                backend.setMusicLibraryPath(path);
+              }
+            },
+          ),
         ],
       ),
     );
