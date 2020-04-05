9bece557c7157a8c344fed3e5fac31726991bf00
Elias Projahn <johrpan@gmail.com>
2020-04-05 11:26:56 +0200

    Remove tracks from recording editor
    The tracks will not be part of the recording entity. Instead, they will
    be managed outside the database alongside the corresponding audio files.

--- lib/editors/recording.dart
+++ lib/editors/recording.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:path/path.dart' as pth;
 
 import '../backend.dart';
 import '../database.dart';
-import '../selectors/files.dart';
 import '../selectors/performer.dart';
 import '../selectors/work.dart';
-
-class TrackModel {
-  final String path;
-
-  TrackModel({
-    this.path,
-  });
-}
 
 class RecordingEditor extends StatefulWidget {
   final Recording recording;
@@ -29,7 +19,6 @@ class RecordingEditor extends StatefulWidget {
 class _RecordingEditorState extends State<RecordingEditor> {
   Work work;
   List<PerformanceModel> performances = [];
-  List<TrackModel> tracks = [];
 
   @override
   void initState() {
@@ -137,47 +126,6 @@ class _RecordingEditorState extends State<RecordingEditor> {
                 onPressed: () {
                   setState(() {
                     performances.remove(performance);
-                  });
-                },
-              ),
-            ),
-          ListTile(
-            title: Text('Tracks'),
-            trailing: IconButton(
-              icon: const Icon(Icons.add),
-              onPressed: () async {
-                final paths = await Navigator.push<Set<String>>(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => FilesSelector(
-                      baseDirectory: backend.musicLibraryPath,
-                    ),
-                    fullscreenDialog: true,
-                  ),
-                );
-
-                if (paths != null) {
-                  setState(() {
-                    for (final path in paths) {
-                      final relPath =
-                          pth.relative(path, from: backend.musicLibraryPath);
-                      tracks.add(TrackModel(
-                        path: relPath,
-                      ));
-                    }
-                  });
-                }
-              },
-            ),
-          ),
-          for (final track in tracks)
-            ListTile(
-              title: Text(track.path),
-              trailing: IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () {
-                  setState(() {
-                    tracks.remove(track);
                   });
                 },
               ),
