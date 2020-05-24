dfeaefd0b3dd6bc996a9d19a97468f03e00a751c
Elias Projahn <johrpan@gmail.com>
2020-05-24 12:10:20 +0200

    common: Placeholder text for no performers

--- common/lib/src/widgets/texts.dart
+++ common/lib/src/widgets/texts.dart
@@ -12,27 +12,30 @@ class PerformancesText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List<String> performanceTexts = [];
+    if (performanceInfos.isEmpty) {
+      return Text('Unknown performers');
+    } else {
+      final List<String> performanceTexts = [];
 
-    for (final p in performanceInfos) {
-      final buffer = StringBuffer();
+      for (final p in performanceInfos) {
+        final buffer = StringBuffer();
 
-      if (p.person != null) {
-        buffer.write('${p.person.firstName} ${p.person.lastName}');
-      } else if (p.ensemble != null) {
-        buffer.write(p.ensemble.name);
-      } else {
-        buffer.write('Unknown');
+        if (p.person != null) {
+          buffer.write('${p.person.firstName} ${p.person.lastName}');
+        } else if (p.ensemble != null) {
+          buffer.write(p.ensemble.name);
+        } else {
+          buffer.write('Unknown');
+        }
+
+        if (p.role != null) {
+          buffer.write(' (${p.role.name})');
+        }
+
+        performanceTexts.add(buffer.toString());
       }
-
-      if (p.role != null) {
-        buffer.write(' (${p.role.name})');
-      }
-
-      performanceTexts.add(buffer.toString());
+      
+      return Text(performanceTexts.join(', '));
     }
-
-    return Text(performanceTexts.join(', '));
   }
 }
-
